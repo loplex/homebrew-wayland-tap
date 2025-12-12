5f28ad1640739af67f1ba26655db956ad77023e8
Martin (Lopin) Lopatář <lop_in_github@dataplex.cz>
2025-12-12 17:19:04 +0100

    unmodified version from linuxbrew

---
+++ Formula/wayland-protocols.rb
@@ -0,0 +1,43 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class WaylandProtocols < Formula
+  desc "Additional Wayland protocols"
+  homepage "https://wayland.freedesktop.org"
+  url "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/archive/1.46/wayland-protocols-1.46.tar.gz"
+  sha256 "83afedde1e63751578ba51b5f6c36052802f85634ed68be41ecfcc3a515ab03d"
+  license "MIT"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => [:build, :test]
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    system "meson", "setup", "build", "-Dtests=false", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    system "pkg-config", "--exists", "wayland-protocols"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test wayland-protocols`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    #system "false"
+  end
+end
+
